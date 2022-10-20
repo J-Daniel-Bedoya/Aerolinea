03efba99998a97b8e763fd14676e968c21f24a04
daniel1718 <jbedoyachavarriaga@gmail.com>
2022-10-20 16:18:01 -0500

    new elements

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,22 +1,42 @@
-import axios from "axios";
+import axios from "axios"; // con esto puedo consumir una api
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import SelecionarPaisDestino from "./SeleccionarPaisDestino";
+import { useForm } from "react-hook-form"; //esta es la librería para formularios
+import SelecionarPaisDestino from "./SeleccionarPaisDestino"; // este es el componente para seleccionar un país
+import SeleccionCantidadPersonas from "./SeleccionCantidadPersonas"; //este es el componente para seleccionar cantidad de personas
 
 const Home = () => {
 
   // esta es la url de la api mymapi
   const urlPais =
     "https://api.mymappi.com/v2/geocoding/reverse?apikey=c829b7f1-0151-42ab-83b8-a7d1c7528d81";
-  const { register, handleSubmit } = useForm();
-  const [paisOrigen, setPaisOrigen] = useState("");
-  const [modalSeleccionPais, setModalSeleccionPais] = useState(false)
-  const [modalSeleccionFecha, setModalSeleccionFechas] = useState(false)
-  const [mostrarSeleccionPais, setMostrarSeleccionPais] = useState('');
+  const { register, handleSubmit } = useForm(); // este es el formulario que contiene todos los datos del vuelo
+  // pais origen y de destino
+  const [paisOrigen, setPaisOrigen] = useState(""); //este contiene el pais en el que recide la persona que hace la recervacion
+  const [mostrarSeleccionPais, setMostrarSeleccionPais] = useState(''); //este contiene el nombre del país seleccionado
+  // total de personas a viajar
+  const [adultos, setAdultos] = useState(null); //este es el total de adultos que viajaran
+  const [niños, setNiños] = useState(null); //este es el total de niños que viajaran
+  const [bebes, setBebes] = useState(null); //este es el total de bebés que viajaran
+  // modales
+  const [modalSeleccionPais, setModalSeleccionPais] = useState(false); // este es el boton del modal para selecionar pais
+  const [modalSeleccionFecha, setModalSeleccionFechas] = useState(false); //este es el boton para selecionar fechas
+  const [modalSeleccionCantidadPersonas, setModalSeleccionCantidadPersonas] = useState(false); //este es el boton para selcionar catidad de personas
+  // esta es la funcion que me llevará a la siguiente página
   const submit = (form) => {
-    console.log(form);
+    const registroVuelo = {
+      "tipoVuelo": form,
+      "paísOrigen": paisOrigen,
+      "Destino": mostrarSeleccionPais,
+      "totalPersonas": {
+        "adultos": adultos,
+        "niños": niños,
+        "bebes": bebes
+      } 
+    }
+    console.log(registroVuelo)
   };
-
+// este useEffect contiene la localización de la persona que hace la recerva
+// es una función predeterminada que se encuentra MDN Docs
   useEffect(() => { 
     navigator.geolocation.getCurrentPosition(success);
     function success(pos) {
@@ -27,6 +47,7 @@ const Home = () => {
       paisOrigenGeolocalizacion(latitud, longitud);
     };
   }, []);
+// esta funcion consume la APPI de mymappi para poder obtener el nombre del país de donde recide la persona
 const paisOrigenGeolocalizacion = (log, lat) => {
   axios.get(`${urlPais}&lat=${log}&lon=${lat}`)
       .then((res) => {
@@ -34,11 +55,19 @@ const paisOrigenGeolocalizacion = (log, lat) => {
       })
       .catch((error) => console.log(error));
   };
-
+// esta funcion cuando se ejecuta me trae el nombre del país elegido
   const seleccionPais = (dato) => {
     setMostrarSeleccionPais (dato)
     setModalSeleccionPais (!modalSeleccionPais)
   }
+// esta funcion cuando se ejecuta me trae el nombre la cantidad de personas que iran de viaje
+  const totalPersonas = (adultos, niños, bebes) => {
+    setModalSeleccionCantidadPersonas(!modalSeleccionCantidadPersonas);
+    setAdultos(adultos);
+    setNiños(niños);
+    setBebes(bebes);
+  };
+
   return (
     <div className="Home">
       <div className="Home__card">
@@ -80,8 +109,13 @@ const paisOrigenGeolocalizacion = (log, lat) => {
             <div className="selection">
               <p>Regreso</p>
             </div>
-            <div className="selection">
-              <p>Cantidad pasajeros</p>
+            <div className="selection" onClick={() => setModalSeleccionCantidadPersonas(!modalSeleccionCantidadPersonas)}>
+              <b>Personas</b>
+              <div>
+                <p>{adultos>0 && ` ${adultos} Adultos`}</p>
+                <p>{niños>0 && `${niños} Niños`}</p>
+                <p>{bebes>0 && `${bebes} Bebés`}</p>
+              </div>
             </div>
             <div className="selection">
               <p>Codigos de promoción</p>
@@ -99,6 +133,11 @@ const paisOrigenGeolocalizacion = (log, lat) => {
         modalSeleccionPais={modalSeleccionPais}
         seleccionPais={seleccionPais}
       />}
+      {modalSeleccionCantidadPersonas && 
+        <SeleccionCantidadPersonas 
+        totalPersonas={totalPersonas}
+        />
+      }
     </div>
   );
 };

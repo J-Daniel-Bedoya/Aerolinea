eff57563dd7a5084b1adea7472ba226ba68b8819
Mateo De Los Rios <mateodelosrios95@gmail.com>
2022-10-22 10:35:57 -0500

    new elements

--- src/components/confirmacionVuelos/ConfirmacionVuelos.jsx
+++ src/components/confirmacionVuelos/ConfirmacionVuelos.jsx
@@ -1,15 +1,84 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'; 
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ConfirmacionVuelos = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Hola</h1>
-      <button onClick={() => navigate("/")}>Ir atras</button>
+    <div className="confirmacion_vuelos">
+      <div className="info_reservacion">
+        <p>Tu reservacion</p>
+        <div></div>
+      </div>
+      <div className="vuelo_salida">
+        <div className="title_vuelo">
+          <h1>Vuelo de salida</h1>
+          <button onClick={() => navigate("/")} className="btn_cambiarVuelo">
+            Cambiar vuelo
+          </button>
+        </div>
+        <div className="title_datos">
+          <p>Martes 30 noviembre 2021</p>
+          <p>Colombia a Brasil</p>
+          <p>Seleccion de horarios y equipajes</p>
+        </div>
+        <div className="info_vuelo">
+          <p>05:50 PM</p>
+          <div>
+            <p>1h 57min</p>
+            <hr />
+            <p>Sin escalas</p>
+          </div>
+          <p>06:47 PM</p>
+          <div className="maleta1">
+            <p>1 objeto personal</p>
+            <p>$120.000</p>
+          </div>
+          <div className="maleta2">
+            <p>Equipaje de mano</p>
+            <p>$240.000</p>
+          </div>
+          <div className="maleta3">
+            <p>Equipaje 25Kg</p>
+            <p>$320.000</p>
+          </div>
+        </div>
+      </div>
+      <div className="vuelo_regreso">
+        <div className="title_vuelo">
+          <h1>Vuelo de regreso</h1>
+          <button onClick={() => navigate("/")} className="btn_cambiarVuelo">
+            Cambiar vuelo
+          </button>
+        </div>
+        <div className="title_datos">
+          <p>Miercoles 8 diciembre 2021</p>
+          <p>Brasil a Colombia</p>
+          <p>Seleccion de horarios y equipajes</p>
+        </div>
+        <div className="info_vuelo">
+          <p>07:28 AM</p>
+          <div>
+            <p>1h 57min</p>
+            <hr />
+            <p>Sin escalas</p>
+          </div>
+          <p>10:08 AM</p>
+          <div className="maleta1">
+            <p>1 objeto personal</p>
+            <p>$120.000</p>
+          </div>
+          <div className="maleta2">
+            <p>Equipaje de mano</p>
+            <p>$240.000</p>
+          </div>
+          <div className="maleta3">
+            <p>Equipaje 25Kg</p>
+            <p>$320.000</p>
+          </div>
+        </div>
+      </div>
     </div>
- 
-  )
-}
- 
+  );
+};
+
 export default ConfirmacionVuelos;
--- src/components/home/Pagos.jsx
+++ src/components/home/Pagos.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+// import imagenPasarelaPagos from "../../assets/imgs/pasareladepago.png";
  
 const Pagos = () => {
   return (
     <div className='Pagos'>
-      <h1>Hola</h1>
+      <h1>Pago seguro</h1>
+      <div>
+        <p>tarjeta de credito, tarjeta de debito y pago electronico </p>
+        {/* <img src={imagenPasarelaPagos} className="imgAvion" /> */}
+      </div>
     </div>
  
   )
